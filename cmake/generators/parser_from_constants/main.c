62c33d79e908e456e5aaa84bd8a7850c6686836c
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-03-19 02:09:56 +0300

    implementing constants generator

--- cmake/generators/parser_from_constants/main.c
+++ cmake/generators/parser_from_constants/main.c
@@ -213,8 +213,23 @@ static inline int get_max_state(size_t* max_state_ptr)
 #define NEXT_STATE_BY_LAST_SYMBOLS_TEMPLATE "[%zu] = %zu"
 #define NEXT_STATE_BY_LAST_SYMBOLS_TERMINATOR ",\n"
 
+static inline bool find_state_by_prefix(size_t* state_ptr, const char* constant, size_t prefix_length)
+{
+  for (size_t index = 0; index < CONSTANTS_LENGTH; index++) {
+    const char* target_constant = CONSTANTS[index];
+
+    if (prefix_length == strlen(target_constant) && strncmp(constant, target_constant, prefix_length) == 0) {
+      *state_ptr = index;
+      return true;
+    }
+  }
+
+  return false;
+}
+
 static inline int print_next_state_by_last_symbols(size_t alphabet_length, size_t max_state)
 {
+  // Each state has alphabet length of possible last symbols.
   size_t next_state_by_last_symbols_length = (max_state + 1) * alphabet_length;
 
   size_t* next_state_by_last_symbols = malloc(next_state_by_last_symbols_length * sizeof(size_t));
@@ -226,26 +241,41 @@ static inline int print_next_state_by_last_symbols(size_t alphabet_length, size_
   size_t index;
 
   for (index = 0; index < next_state_by_last_symbols_length; index++) {
+    // Initialize all next states with initial state.
     next_state_by_last_symbols[index] = INITIAL_STATE;
   }
 
   size_t global_state = INITIAL_STATE;
 
   for (index = 0; index < CONSTANTS_LENGTH; index++) {
+    // Starting with empty string and initial state.
     const char* constant = CONSTANTS[index];
     size_t      state    = INITIAL_STATE;
 
     for (size_t jndex = 0; jndex < strlen(constant); jndex++) {
-      uint8_t last_symbol                           = symbol_by_bytes[constant[jndex]];
-      size_t  last_symbol_index                     = state * alphabet_length + last_symbol;
-      next_state_by_last_symbols[last_symbol_index] = ++global_state;
+      uint8_t last_symbol       = symbol_by_bytes[constant[jndex]];
+      size_t  last_symbol_index = state * alphabet_length + last_symbol;
+      size_t  last_symbol_state = next_state_by_last_symbols[last_symbol_index];
 
-      state = global_state;
+      if (last_symbol_state == INITIAL_STATE) {
+        // We need to check whether current prefix matches constant.
+        if (!find_state_by_prefix(&state, constant, jndex + 1)) {
+          state = ++global_state;
+        }
+
+        next_state_by_last_symbols[last_symbol_index] = state;
+      }
+      else {
+        state = last_symbol_state;
+      }
     }
   }
 
   if (global_state != max_state) {
     PRINT_ERROR("global state is not equal to max state\n");
+
+    free(next_state_by_last_symbols);
+
     return 2;
   }
 
