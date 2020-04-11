3ec04068ca90be4d3216371fd0765e0839dc2a55
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-04-11 17:18:36 +0300

    updated request research results

--- scripts/request-collector/process_requests/requests.rb
+++ scripts/request-collector/process_requests/requests.rb
@@ -12,9 +12,9 @@ LOG_PATH       = File.join(TEMP_DIRECTORY, "log").freeze
 # Request: "GET /a/b HTTP/1.0" 200 .
 REQUEST_REGEXP = Regexp.new(
   "
-    \"
+    ['\"]
       (
-        [^\"[:space:]]+
+        [^'\"[:space:]]+
       )
       [ ]
 
@@ -29,7 +29,7 @@ REQUEST_REGEXP = Regexp.new(
         |
           1\.1
       )
-    \"
+    ['\"]
     [ ]
 
     [123]
