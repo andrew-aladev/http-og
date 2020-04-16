d0993a2f4a0c7ef63a4d5535ede13fbc18edeb19
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-04-17 01:49:53 +0300

    cosmetics

---
+++ src/processor/version/1_0_or_1_1/main.h
@@ -0,0 +1,48 @@
+// HTTP old generation (v0.9, v1.0, v1.1) C library.
+// Copyright (c) 2019 AUTHORS, MIT License.
+
+// This file was generated, do not edit manually.
+
+// It may not be possible to run generator while cross compiling.
+// So generated file should be shipped with its template.
+
+#if !defined(HOG_PROCESSOR_VERSION_1_0_or_1_1_H)
+#define HOG_PROCESSOR_VERSION_1_0_or_1_1_H
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+#include "../../../common.h"
+#include "../../../log.h"
+
+typedef uint8_t hog_processor_version_1_0_or_1_1_state_t;
+
+extern const hog_processor_state_fast_t HOG_PROCESSOR_VERSION_1_0_or_1_1_INITIAL_STATE;
+
+extern const size_t HOG_PROCESSOR_VERSION_1_0_or_1_1_CONSTANTS_LENGTH;
+extern const size_t HOG_PROCESSOR_VERSION_1_0_or_1_1_ALPHABET_LENGTH;
+extern const size_t HOG_PROCESSOR_VERSION_1_0_or_1_1_ALPHABET_MAX_LENGTH;
+
+extern const hog_symbol_fast_t HOG_PROCESSOR_VERSION_1_0_or_1_1_UNDEFINED_SYMBOL;
+extern const hog_symbol_t      HOG_PROCESSOR_VERSION_1_0_or_1_1_SYMBOL_BY_BYTES[];
+
+extern const hog_processor_version_1_0_or_1_1_state_t HOG_PROCESSOR_VERSION_1_0_or_1_1_NEXT_STATE_BY_LAST_SYMBOLS[];
+
+inline hog_processor_state_fast_t hog_processor_version_1_0_or_1_1_get_next_state(
+  hog_processor_state_fast_t state, hog_symbol_fast_t byte)
+{
+  hog_symbol_fast_t symbol = HOG_PROCESSOR_VERSION_1_0_or_1_1_SYMBOL_BY_BYTES[byte];
+  if (HOG_PROCESSOR_VERSION_1_0_or_1_1_ALPHABET_LENGTH != HOG_PROCESSOR_VERSION_1_0_or_1_1_ALPHABET_MAX_LENGTH && symbol == HOG_PROCESSOR_VERSION_1_0_or_1_1_UNDEFINED_SYMBOL) {
+    HOG_LOG_ERROR("version_1_0_or_1_1 processor received invalid byte: %u", byte);
+    return HOG_PROCESSOR_VERSION_1_0_or_1_1_INITIAL_STATE;
+  }
+
+  return HOG_PROCESSOR_VERSION_1_0_or_1_1_NEXT_STATE_BY_LAST_SYMBOLS[state * HOG_PROCESSOR_VERSION_1_0_or_1_1_ALPHABET_LENGTH + symbol];
+}
+
+inline bool hog_processor_version_1_0_or_1_1_is_finished(hog_processor_state_fast_t state)
+{
+  return state < HOG_PROCESSOR_VERSION_1_0_or_1_1_CONSTANTS_LENGTH;
+}
+
+#endif // HOG_PROCESSOR_VERSION_1_0_or_1_1_H
---
+++ src/processor/version/any/options.c
@@ -0,0 +1,19 @@
+// HTTP old generation (v0.9, v1.0, v1.1) C library.
+// Copyright (c) 2019 AUTHORS, MIT License.
+
+// This file is intended to be used by generator only.
+
+#include "options.h"
+
+#include <stdint.h>
+
+// Any bytes except separator or terminator.
+
+const hog_alphabet_range HOG_ALPHABET_RANGES[]      = {{0, UINT8_MAX}};
+const size_t             HOG_ALPHABET_RANGES_LENGTH = sizeof(HOG_ALPHABET_RANGES) / sizeof(HOG_ALPHABET_RANGES[0]);
+
+const char   HOG_ALPHABET[]      = {' ', '\r', '\n'};
+const size_t HOG_ALPHABET_LENGTH = sizeof(HOG_ALPHABET) / sizeof(HOG_ALPHABET[0]);
+
+const size_t HOG_MIN_LENGTH = 1;
+const size_t HOG_MAX_LENGTH = UINT8_MAX;
