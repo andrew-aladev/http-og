11ee7fb0b39da6c45da7f820f3cb496b9f371a71
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-05-08 09:46:26 +0300

    implementing header generators and processors

---
+++ src/processor/any_visible_text_except_separator/options.c
@@ -0,0 +1,24 @@
+// HTTP old generation (v0.9, v1.0, v1.1) C library.
+// Copyright (c) 2019 AUTHORS, MIT License.
+
+// This file was generated, do not edit manually.
+// This file is intended to be used by generator only.
+
+// It may not be possible to run generator while cross compiling.
+// So generated file should be distributed with its template.
+
+#include "options.h"
+
+// RFC 1945 - 2.2 Basic Rules.
+// RFC 7230 - Appendix B. Collected ABNF.
+
+const hog_alphabet_range_t HOG_ALPHABET_RANGES[]      = {{'\x21', '\x7e'}, {'\x80', '\xff'}};
+const size_t               HOG_ALPHABET_RANGES_LENGTH = sizeof(HOG_ALPHABET_RANGES) / sizeof(HOG_ALPHABET_RANGES[0]);
+
+const char   HOG_ALPHABET[]      = {0};
+const size_t HOG_ALPHABET_LENGTH = 0;
+
+const hog_alphabet_mode_t HOG_ALPHABET_MODE = HOG_ALPHABET_INCLUDING_BYTES_INTO_RANGES;
+
+const size_t HOG_MIN_LENGTH = 1;
+const size_t HOG_MAX_LENGTH = UINT16_MAX;
